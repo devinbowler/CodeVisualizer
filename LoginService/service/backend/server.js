3d07067844685d9b9e54b8bb0da5678d9b6255a6
Devin Bowler <83326036+devinbowler@users.noreply.github.com>
2025-03-09 19:16:03 -0400

    Update server.js

--- LoginService/service/backend/server.js
+++ LoginService/service/backend/server.js
@@ -57,6 +57,7 @@ app.post("/registerUser", async (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  // console.log(`Server is running on http://localhost:${port}`);
+  console.log('Server is running.');
 });
  
